5c2b9f75ce88f2adf16e8e2a6084da7d13052e72
PJCB1998 <pjcb1998@hotmail.com>
2023-03-01 08:37:31 -0500

    Add Statring Files

---
+++ src/main/java/ruproject/RuprojectApplication.java
@@ -0,0 +1,13 @@
+package ruproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuprojectApplication.class, args);
+	}
+
+}
